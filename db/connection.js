43d81a57f46a9cd946bba54d5dfcb5f5d9b83bbd
John Hutcheon <johnhutcheon@Johns-MacBook-Pro.local>
2023-11-09 15:58:54 +0000

    changes to get articles model, to help with front end

--- db/connection.js
+++ db/connection.js
@@ -9,10 +9,10 @@ require("dotenv").config({
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
-}
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
+  if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set");
+  }
 }
 
 module.exports = new Pool(config);
--- models/models.js
+++ models/models.js
@@ -51,7 +51,7 @@ exports.fetchArticles = (
     return Promise.reject({ status: 400, message: "Invalid Query" });
   }
 
-  let queryStr = `SELECT articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count
+  let queryStr = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count
 FROM articles
 LEFT JOIN comments ON comments.article_id = articles.article_id`;
 
